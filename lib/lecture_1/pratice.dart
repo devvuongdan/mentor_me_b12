bb57a33e24dfb0c87294242dff1528e2c53f89c5
VuongDan <dev.vuongdan@gmail.com>
2023-02-13 08:42:48 +0700

    [Dev.VuongDan] update lecture 2

--- lib/lecture_1/pratice.dart
+++ lib/lecture_1/pratice.dart
@@ -1,1 +1,65 @@
+void main() async {
+  // int a = 10;
 
+  // int b = 10;
+
+  // print(a == b);
+
+  // NewClass? c;
+
+  // NewClass? d;
+
+  // print(c is NewClass?);
+  // print(d.hashCode);
+
+  // hashCode
+  // value
+  // runtimeType
+
+  // final a = NewClass(name: "aaa") as NewClass;
+
+  // final NewClass a = NewClass(name: "aaa");
+
+  // print(a.name);
+
+  // var grapes;
+
+  // (grapes as NewClass).name = "AAAA";
+
+  //Condition A
+  //Condition B
+
+  // print((a > 0) && (b > a));
+
+  // print((a > 0) && (b == a));
+
+  // print((a > 0) || (b == a));
+
+  // print(!(a > 0));
+
+  // bool value = false;
+
+  // bool value1 = !value;
+
+  // print(value1);
+
+  // A right
+  // B right
+  // => A && B right;
+
+  // int a = 4;
+  // int b = 5;
+
+  // bool value1 = a > 0;
+
+  // bool value2 = b < 0;
+
+  // print(!value1 && !value2);
+}
+
+class NewClass {
+  String name;
+  NewClass({
+    required this.name,
+  });
+}
